ecd91706963304e485f96b388977836d63aab6d6
oxanaP <oxichka@list.ru>
2024-02-01 22:21:26 +0700

    Initial commit

---
+++ src/BonusMilesService.java
@@ -0,0 +1,8 @@
+public class BonusMilesService {
+    public int calculate(int aPrice) {
+        int result;
+        int oneMily = 20;
+        result = aPrice / oneMily;
+        return result;
+    }
+}
